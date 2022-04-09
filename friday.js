eab18749eeefffa5262d0645c1f93c1ec93485b6
lbourke1234 <lbourke1234@gmail.com>
2022-04-09 22:15:34 +0100

    saturday push

--- friday.js
+++ friday.js
@@ -1,82 +1,58 @@
+// global variables
 let bodyInfo = []
 
-let artists = []
+let artists = [
+    'metallica',
+    'ed sheeran',
+    'alter bridge',
+    'eminem',
+    'tupac',
+    'biggie smalls',
+    'opeth',
+    'joe bonamassa'
+]
+
+let rowNode = document.querySelector('.friday-row')
+rowNode.innerHTML = ''
 
 // arrayOfSongs = Promise.All(artists.forEach(artist => fetchSongs(artist.name)))
 // renderSongs(arrayOfSongs)
 
 window.onload = () => {
-    // fetchAlbumByID()
-    renderCards('metallica')
-    renderCards('ed sheeran')
+    renderCards()
 }
 
-// const fetchAlbumByID = () => {
-//     fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=queen')
-//     .then(res => res.json())
-//     .then(body => {
-//         bodyInfo = body
-//         console.log(body)
-//     })
-// }
-
-// const findIDinAPI = (id) => {
-//     // if (id === )
-//     fetch()
-// }
-
-
-const renderCards = query => {
-
-    // let rowNode = document.querySelector('.friday-row')
-    
-    // artists.forEach(query => {
-
-    fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=' + query)
+const fetchArtists = (artist) => {
+    fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=' + artist)
     .then(res => res.json())
     .then(body => {
-        // console.log(body.data)
-        artists = body.data
-        console.log(body)
-        
-        let rowNode = document.querySelector('.friday-row')
-        // rowNode.innerHTML = ''
-
-        
-        // rowNode.classList.add('second-section-card')
-
-
-        // let colNode = document.createElement('div')
-        // colNode.classList.add('col')
-
-        rowNode.innerHTML += `<div id="${body.id}" class="col">
-                                <div class="card second-section-card">
-                                    <img src="${body.cover}" class="card-img-top img-second-section" alt="...">
-                                    <div class="card-body card-body-section2">
-                                    <a href="./artist.html?picId=${body.id}"><h5 class="card-title">${body.title}</h5></a>
-                                        <p class="card-text" >${body.artist.name}</p>
-                                    </div>
-                                </div>
-                            </div>
-                            `
+        // console.log(body.data[0].album.title)
+        createNewCard(body.data[0], body.data)
     })
-                     
 }
 
+const renderCards = () => {
+    artists.forEach(artist => fetchArtists(artist))
+}                   
 
-// const clickAlbum = (id) => {
-//     fetch('https://striveschool-api.herokuapp.com/api/deezer/album/' + id)
-//     .then(res => res.json())
-//     .then(body => {
-//         location.href = "artist.html"
-//         console.log(body)
+const createNewCard = (body) => {
+    let cardNode = document.createElement('div')
+    console.log(body)
+    cardNode.innerHTML += `
+                        <div class="col">
+                            <div class="card second-section-card">
+                                <img src="${body.album.cover}" class="card-img-top img-second-section" alt="...">
+                                <div class="card-body card-body-section2">
+                                    <h5 class="card-title">${body.album.title}</h5>
+                                    <p class="card-text">${body.artist.name}</p>
+                                </div>
+                            </div>
+                        </div>
+                        `
+    appendCardToRow(cardNode.innerHTML)
+}
 
-
-
-//         let artistText = document.querySelector('.jumbotron h1')
-//         console.log(artistText.innerText)
-
-
-//     })
-// }
-
+const appendCardToRow = (card) => {
+    rowNode.innerHTML += card
+    // console.log(rowNode)
+}
---
+++ fridayArtist.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+
+}
+
+const changeArtistInfoOnClick = (id) => {
+    
+}
